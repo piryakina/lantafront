5db4fc5c0e52d5358bbd2efda1ad614f543a7cde
piryakina <anya.piryakina@gmail.com>
2022-12-26 15:25:51 +0300

    changes front

--- src/app/system/sp/sp.component.ts
+++ src/app/system/sp/sp.component.ts
@@ -82,7 +82,7 @@ export class SpComponent implements OnInit {
     //     console.log(true)
     //   }
     // }
-    console.log(this.fileusp)
+    // console.log(this.fileusp)
   }
 
   compare(object1: IFile, object2: IFile) {
