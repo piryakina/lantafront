0aefa6264ee02f68a3737c6311bd174b51298735
piryakina <anya.piryakina@gmail.com>
2022-12-30 09:38:00 +0300

    mmm

--- src/app/app.guard.ts
+++ src/app/app.guard.ts
@@ -3,12 +3,12 @@ import {Observable} from "rxjs";
 import {LocalStorageService} from "ngx-webstorage";
  
 export class AppGuard implements CanActivate{
-    constructor(private storage:LocalStorageService){}
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) : Observable<boolean> | boolean{
+   
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot, storage:LocalStorageService) : Observable<boolean> | boolean{
         if (this.storage.retrieve("id")!==undefined){
             return true
         } else {
-            window.location.href=["/"]
+            window.location.href="/"
         return false
         }
     }
