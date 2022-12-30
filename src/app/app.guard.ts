2fac3da2475480987bba21da86068b5cd2e2b815
piryakina <anya.piryakina@gmail.com>
2022-12-30 09:57:52 +0300

    mmm

--- src/app/app.guard.ts
+++ src/app/app.guard.ts
@@ -9,7 +9,7 @@ export class AppGuard implements CanActivate{
     constructor(private storage: LocalStorageService) {}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) : boolean{
         console.log(this.storage.retrieve("id"))
-        if (this.storage.retrieve("id")!==undefined){
+        if (this.storage.retrieve("id")!==null){
 
             return true
         } else {
