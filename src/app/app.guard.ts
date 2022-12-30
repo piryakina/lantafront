be6df6394119edd191f14ebf83aa6be27ea9734b
piryakina <anya.piryakina@gmail.com>
2022-12-30 09:45:34 +0300

    mmm

--- src/app/app.guard.ts
+++ src/app/app.guard.ts
@@ -1,7 +1,10 @@
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
 import {Observable} from "rxjs";
 import {LocalStorageService} from "ngx-webstorage";
+import {Injectable, Inject} from '@angular/core';
+
  
+ @Injectable()
 export class AppGuard implements CanActivate{
     constructor(
     @Inject(LocalStorageService) private auth: storage
